709c241f5045ed9a086665692ef5aae9255a150b
Mathias Kahl <mathias.kahl.ext@hermesworld.com>
2016-09-14 02:21:40 +0200

    extended coast tileset

--- src/shaders/land.fragment.glsl
+++ src/shaders/land.fragment.glsl
@@ -42,7 +42,7 @@ void main() {
     vec3 color = lightAmbient * texColor.xyz + lambertian * texColor.xyz * lightDiffuse;
     gl_FragColor = vec4(color, 1.0);
 
-    if (vExtra > 0.97 && false) { // hex border
+    if (vExtra > 0.97 && true) { // hex border
         float f = clamp(0.5 * vExtra - zoom * 0.005, 0.0, 1.0); //0.8;
         f = 0.34;
         gl_FragColor = mix(vec4(.9, .9, .7, 1.0), gl_FragColor, 1.0 - f);
