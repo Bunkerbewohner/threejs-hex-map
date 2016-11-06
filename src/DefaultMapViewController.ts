2b675a6d7ec9573abebac195eb47679e3cc0514c
Mathias Kahl <mathias.kahl.ext@hermesworld.com>
2016-11-06 11:43:04 +0100

    bugfix

--- src/DefaultMapViewController.ts
+++ src/DefaultMapViewController.ts
@@ -9,6 +9,7 @@ export default class Controller implements MapViewController {
     private pickingCamera: Camera
     private mouseDownPos: Vector3
     private dragStartCameraPos: Vector3
+    private lastDrag: Vector3 = new Vector3(0, 0, 0)
 
     init(controls: MapViewControls, canvas: HTMLCanvasElement) {
         this.controls = controls        
@@ -16,7 +17,7 @@ export default class Controller implements MapViewController {
         canvas.addEventListener("mousedown", this.onMouseDown, false)
         canvas.addEventListener("mousemove", this.onMouseMove, false)
         canvas.addEventListener("mouseup", this.onMouseUp, false)
-        canvas.addEventListener("mouseout", this.onMouseUp, false)
+        canvas.addEventListener("mouseout", this.onMouseOut, false)
     }
 
     onMouseDown = (e: MouseEvent) => {
@@ -28,7 +29,7 @@ export default class Controller implements MapViewController {
     onMouseMove = (e: MouseEvent) => {
         if (this.mouseDownPos) {
             const mousePos = screenToWorld(e.clientX, e.clientY, this.pickingCamera)
-            const dv = mousePos.sub(this.mouseDownPos).multiplyScalar(-1)
+            const dv = this.lastDrag = mousePos.sub(this.mouseDownPos).multiplyScalar(-1)
 
             const newCameraPos = dv.clone().add(this.dragStartCameraPos)
             this.controls.getCamera().position.copy(newCameraPos)
@@ -36,6 +37,18 @@ export default class Controller implements MapViewController {
     }
 
     onMouseUp = (e: MouseEvent) => {
-        this.mouseDownPos = null
+        this.mouseDownPos = null // end drag
+        
+        if (this.lastDrag.length() < 1) {
+            const mousePos = screenToWorld(e.clientX, e.clientY, this.controls.getCamera())
+            const tile = this.controls.pickTile(mousePos)
+            if (tile) {
+                this.controls.selectTile(tile)
+            }
+        }
+    }
+
+    onMouseOut = (e: MouseEvent) => {
+        this.mouseDownPos = null // end drag
     }
 }
