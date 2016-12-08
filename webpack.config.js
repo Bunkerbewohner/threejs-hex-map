401c0582561c92e2614e3fff9b2cd0dad8bd3edc
Mathias Kahl <mathias.kahl@googlemail.com>
2016-12-08 19:42:35 +0100

    webpack library conf with amd

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ module.exports = {
     entry: "./lib/src/index.js",
     output: {
         path: __dirname + "/dist/",
-        filename: "threejs-hex-map.js"
+        filename: "threejs-hex-map.js",
+        library: "threejs-hex-map",
+        libraryTarget: "amd"
     },
     externals: {
         "three": "THREE"
